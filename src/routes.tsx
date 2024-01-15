ebc2f480be63e49f352e02f5ac3ddf02b993d818
Davi <davi.frrs@gmail.com>
2024-01-15 17:28:31 -0300

    feat: config react router dom

---
+++ src/routes.tsx
@@ -0,0 +1,19 @@
+import { createBrowserRouter } from 'react-router-dom'
+
+import { AuthLayout } from './pages/_layouts/auth'
+import { AppLayout } from './pages/_layouts/main'
+import { Dashboard } from './pages/app/dashboard'
+import { SignIn } from './pages/auth/sign-in'
+
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <AppLayout />,
+    children: [{ path: '/', element: <Dashboard /> }],
+  },
+  {
+    path: '/',
+    element: <AuthLayout />,
+    children: [{ path: '/sign-in', element: <SignIn /> }],
+  },
+])
