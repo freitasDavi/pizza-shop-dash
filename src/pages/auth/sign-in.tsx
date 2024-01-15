13f580238fdee6411b44e1509255d21db6bb785b
Davi <davi.frrs@gmail.com>
2024-01-15 17:57:53 -0300

    feat: toast toast

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,6 +1,7 @@
 import { zodResolver } from '@hookform/resolvers/zod'
 import { Helmet } from 'react-helmet-async'
 import { useForm } from 'react-hook-form'
+import { toast } from 'sonner'
 import { z } from 'zod'
 
 import { Button } from '@/components/ui/button'
@@ -23,8 +24,19 @@ export function SignIn() {
   })
 
   async function handleSignIn(data: SignInForm) {
-    console.log(data)
-    await new Promise((resolve) => setTimeout(resolve, 2000))
+    try {
+      console.log(data)
+      await new Promise((resolve) => setTimeout(resolve, 2000))
+
+      toast.success('Enviamos um link de autenticação para o seu e-mail!', {
+        action: {
+          label: 'Reenviar link',
+          onClick: () => handleSignIn(data),
+        },
+      })
+    } catch (err) {
+      toast.error('Ocorreu um erro ao enviar o link de autenticação!')
+    }
   }
 
   return (
