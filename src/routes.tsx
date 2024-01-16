df9564201dfc6c8d4e16ffcd455b86a0f79f4cb3
Davi <davi.frrs@gmail.com>
2024-01-16 09:57:54 -0300

    feat: dash header

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter } from 'react-router-dom'
 
+import { AppLayout } from './pages/_layouts/app'
 import { AuthLayout } from './pages/_layouts/auth'
-import { AppLayout } from './pages/_layouts/main'
 import { Dashboard } from './pages/app/dashboard'
 import { SignIn } from './pages/auth/sign-in'
 import { SignUp } from './pages/auth/sign-up'
